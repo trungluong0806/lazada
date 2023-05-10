f9939058b2025eef6ab931b9b29f8f40cf7504e1
huchuu <88017058+GayeongLee21@users.noreply.github.com>
2023-05-10 15:06:27 +0700

    Add files via upload

---
+++ full stack project/vendor/database.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const upload = multer();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const uri = 'mongodb+srv://trungluong0806:Lacussaber080699@cluster0.w8zcmxn.mongodb.net/test?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const dbName = 'test';
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', handleIndex);
+app.get('/products/:id', handleProduct);
+app.get('/add', handleAddForm);
+app.post('/add', upload.single('image'), handleAddProduct);
+app.get('/viewmyproduct', handleViewMyProduct);
+
+async function handleIndex(req, res) {
+  try {
+    await client.connect();
+    const collection = client.db(dbName).collection('product_infos');
+    const docs = await collection.find().toArray();
+    res.render('index', { products: docs });
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    // await client.close();
+  }
+}
+
+async function handleProduct(req, res) {
+  try {
+    await client.connect();
+    const collection = client.db(dbName).collection('product_infos');
+    const product = await collection.findOne({ _id: ObjectId(req.params.id) });
+    res.render('product', { product: product });
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    // await client.close();
+  }
+}
+
+function handleAddForm(req, res) {
+  res.render('add', { title: 'Add New Product' });
+}
+
+async function handleAddProduct(req, res) {
+  try {
+    const { productName, productPrice, productBrand, productLocation, productCFO, productDescription } = req.body;
+    const image = req.body.image;
+
+    await client.connect();
+    const collection = client.db(dbName).collection('product_infos');
+    const result = await collection.insertOne({ productName, productPrice, productBrand, productLocation, productCFO, productDescription, image });
+    console.log(result);
+    res.redirect('/');
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+
+
+async function handleViewMyProduct(req, res) {
+  try {
+    await client.connect();
+    const collection = client.db(dbName).collection('product_infos');
+    const docs = await collection.find().toArray();
+    res.render('viewmyproduct', { products: docs });
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    // await client.close();
+  }
+}
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
