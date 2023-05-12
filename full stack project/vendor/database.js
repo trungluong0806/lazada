22bef5dafd37eea9809d8ae973ce4e4470bff8a4
huchu <88017058+GayeongLee21@users.noreply.github.com>
2023-05-12 21:14:28 +0700

    Update database.js
    change to mongoose

--- full stack project/vendor/database.js
+++ full stack project/vendor/database.js
@@ -3,22 +3,40 @@ const app = express();
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, 'public/uploads/');
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
   }
 });
 const upload = multer({ storage: storage });
-const { MongoClient } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
+const mongoose = require('mongoose');
 const uri = 'mongodb+srv://trungluong0806:Lacussaber080699@cluster0.w8zcmxn.mongodb.net/test?retryWrites=true&w=majority';
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const dbName = 'test';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('MongoDB connected!');
+});
+
+const productSchema = new mongoose.Schema({
+  productName: String,
+  productPrice: Number,
+  productBrand: String,
+  productLocation: String,
+  productCFO: String,
+  productDescription: String,
+  imagePath: String
+});
+
+const Product = mongoose.model('Product', productSchema);
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/uploads', express.static('public/uploads'));
+
 
 app.get('/', handleIndex);
 app.get('/products/:id', handleProduct);
@@ -28,27 +46,19 @@ app.get('/viewmyproduct', handleViewMyProduct);
 
 async function handleIndex(req, res) {
   try {
-    await client.connect();
-    const collection = client.db(dbName).collection('product_infos');
-    const docs = await collection.find().toArray();
+    const docs = await Product.find({});
     res.render('index', { products: docs });
   } catch (err) {
     console.log(err.stack);
-  } finally {
-    // await client.close();
   }
 }
 
 async function handleProduct(req, res) {
   try {
-    await client.connect();
-    const collection = client.db(dbName).collection('product_infos');
-    const product = await collection.findOne({ _id: ObjectId(req.params.id) });
+    const product = await Product.findById(req.params.id);
     res.render('product', { product: product });
   } catch (err) {
     console.log(err.stack);
-  } finally {
-    // await client.close();
   }
 }
 
@@ -59,32 +69,23 @@ function handleAddForm(req, res) {
 async function handleAddProduct(req, res) {
   try {
     const { productName, productPrice, productBrand, productLocation, productCFO, productDescription } = req.body;
-    const imagePath = req.file.path; 
+    const imagePath = req.file.path;
     
-    await client.connect();
-    const collection = client.db(dbName).collection('product_infos');
-    const result = await collection.insertOne({ productName, productPrice, productBrand, productLocation, productCFO, productDescription, imagePath }); // 이미지 경로 저장
-    console.log(result);
+    const newProduct = new Product({ productName, productPrice, productBrand, productLocation, productCFO, productDescription, imagePath });
+    await newProduct.save();
+    
     res.redirect('/');
   } catch (err) {
     console.log(err.stack);
-  } finally {
-    await client.close();
   }
 }
 
-
-
 async function handleViewMyProduct(req, res) {
   try {
-    await client.connect();
-    const collection = client.db(dbName).collection('product_infos');
-    const docs = await collection.find().toArray();
+    const docs = await Product.find({});
     res.render('viewmyproduct', { products: docs });
   } catch (err) {
     console.log(err.stack);
-  } finally {
-    // await client.close();
   }
 }
 
